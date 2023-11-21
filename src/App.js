d06dcf276743ecc508a21edafd08958a4d6a1c23
affaf04 <affaftaouti@gmail.com>
2023-11-21 14:26:53 -0500

    api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
+
 
 function App() {
+  const inputRef = useRef(null)
+  const [searchedStudent, setSearchedStudent] = useState('')
+
+  const handelChange =() => {
+    let content = inputRef.current.value
+
+    setSearchedStudent (content)
+  }
+  const students =[
+    { name: "Jhon", grade: '86%' },
+    { name: "David", grade: "100%" },
+    { name: "Sun", grade: '70% '},
+    { name: "Noah", grade: '45%' },
+    { name: "Foufa", grade: '80%' },
+    { name: "Adam", grade: '30%' },
+    { name: "Joury", grade: '100%' },
+    
+]
+const studentJSX = students.map((student, index) => {
+  return (
+    <div className='rec' key={index}>
+    <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <h3>{student.name}</h3>
+          <h3>{student.grade}</h3>
+        </div>
+      </div>
+    );
+  });
+  const searchedStudentJSX = students
+  .filter(student => student.name === searchedStudent)
+  .map((student, index) => (
+    <div className='rec' key={index}>
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <h3>{student.name}</h3>
+        <h3>{student.grade}</h3>
+      </div>
+    </div>
+  ));
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h2> Grades </h2>
+      {searchedStudent && searchedStudentJSX}
+
+       <input type='text' ref={inputRef} />
+      
+       <button onClick={handelChange} >Click here</button>
+       
+      </div>
   );
 }
 
